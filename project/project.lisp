5e73706808ff86d49f24dd817aca6d764d794cbb
krypticwolf3 <krypticwolf3@users.noreply.github.com>
2016-07-23 08:14:58 -0400

    Formatting whitespace only.
    Indenting for readability, etc., to ease use and testing.

--- project/project.lisp
+++ project/project.lisp
@@ -380,35 +380,34 @@
 ;;Matt Carlstrom
 (let ((explored '()))
 
-(defun has-cycle-rec (map parent)
+  (defun has-cycle-rec (map parent)
   
-  ;set children to the list of nodes connected to parent
-  (let ((children (second (find-if #'(lambda (x) (equal (first x) parent)) map))))
-    ;add parent to the list of explored nodes
-(setf explored (cons parent explored))
-(cond ((null map) nil) ;if map is nil, then we've explored everything
-          (t (let ((cycle-found nil));otherwise
-             ;perform has-cycle-rec on each of the children
- ;until we've reached a node we have already explored (return false)
- (dolist (e children cycle-found)
-(if (and (not (equal parent e)) (find e explored))
-(setf cycle-found t)
-(has-cycle-rec map e)
-)
- )
-)
+    ;set children to the list of nodes connected to parent
+    (let ((children (second (find-if #'(lambda (x) (equal (first x) parent)) map))))
+      ;add parent to the list of explored nodes
+      (setf explored (cons parent explored))
+      (cond ((null map) nil) ;if map is nil, then we've explored everything
+        (t (let ((cycle-found nil));otherwise
+            ;perform has-cycle-rec on each of the children
+            ;until we've reached a node we have already explored (return false)
+            (dolist (e children cycle-found)
+              (if (and (not (equal parent e)) (find e explored))
+                (setf cycle-found t)
+                (has-cycle-rec map e)
+              )
+            )
+          )
+        )
+      )
+    )
   )
-)
-  )
-)
  
- ;wrapper function that starts out has-cycle-rec
-(defun has-cycle (map)
-;start out parent as the very first node that will be explored
-  (let ((return_val (has-cycle-rec map (car (car map)))))
-    (setf explored '())
-    return_val
+  ;wrapper function that starts out has-cycle-rec
+  (defun has-cycle (map)
+  ;start out parent as the very first node that will be explored
+    (let ((return_val (has-cycle-rec map (car (car map)))))
+      (setf explored '())
+      return_val
+    )
   )
-)
-
 )
